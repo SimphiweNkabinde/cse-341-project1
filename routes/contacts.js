715316c1e4ed6c2f4fe89c3eb395e5beca2fe808
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2026-01-14 22:54:33 +0200

    swagger endpoint manual capture

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,5 +4,28 @@ const contactsController = require("../controllers/contacts");
 
 router.get("/", contactsController.getAll);
 router.get("/:id", contactsController.getOne);
+router.post("/", (req, res) => {
+  /** #swagger.auto = true
+   *
+   * #swagger.produces = ['application/json']
+   * #swagger.consumes = ['application/json']
+   * #swagger.parameters['body'] = {
+   *      in: 'body',
+   *      description: 'contact data.',
+   *      required: true,
+   *      schema: {
+   *          firstName: 'first name',
+   *          lastname: 'last name',
+   *          email: 'email address',
+   *          favoriteColor: 'favourite color',
+   *          birthday: 'birth date as yyy-mm-dd'
+   *      }
+   * }
+   *
+   * #swagger.responses[201] = { description: 'contact created successfully' }
+   * #swagger.responses[500] = { description: 'Server failure.'}
+   */
+  return res.send();
+});
 
 module.exports = router;
