07ab6b5d670bc4fabf4c5ff71711ff92318c6820
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2026-01-14 22:39:56 +0200

    Implement Swagger Autogen & Swagger UI

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
 router.get("/", (req, res) => res.send("hello world"));
 
 router.use("/contacts", require("./contacts"));
+
+// SWAGGER
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
